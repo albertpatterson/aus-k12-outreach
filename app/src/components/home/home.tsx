2c9fd5e8a1c382c5271c3692494b0d311b0d4432
albert patterson <apatterson189@gmail.com>
2025-05-22 21:21:44 -0500

    update home page and favicon

--- app/src/components/home/home.tsx
+++ app/src/components/home/home.tsx
@@ -1,9 +1,29 @@
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-      <p>Welcome to the K12 Outreach program!</p>
-      <p>Here you can find information about our events and contacts.</p>
+    <div style={{ maxWidth: '400px', margin: '0 auto', padding: '16px' }}>
+      <p>
+        Find a fun opportunity to engage with our local schools and support our
+        young people around Austin!
+      </p>
+      <ul style={{ textAlign: 'left' }}>
+        <li>
+          <a href="/event-list">Event List</a> to see all upcoming events.
+        </li>
+        <li>
+          <a href="/event-map">Event Map</a> for a goegraphic view of events.
+        </li>
+        <li>
+          <a href="/contacts">Contacts</a> reach out for more information.
+        </li>
+      </ul>
+      <br />
+      <br />
+      <p>
+        Raw data available{' '}
+        <a href="https://docs.google.com/spreadsheets/d/1jQ-Rou08NcR_7GLK1twpi4rGgp0L1DHTIOdJdI2nF-A/edit?gid=164710446#gid=164710446">
+          here
+        </a>
+      </p>
     </div>
   );
 }
